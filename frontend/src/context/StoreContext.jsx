9d7766c37b92ae38b5b68194f2c460633c627282
Kunal Shinde <113423208+kunal22823@users.noreply.github.com>
2026-01-05 01:08:39 +0530

    Update StoreContext.jsx
    reder

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://plants-shop-backend.onrender.com";
   const [plant_list,setPlant_list] = useState([]);
 
   // Initialize token from localStorage only once
